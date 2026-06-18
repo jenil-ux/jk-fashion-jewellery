7c0f2b77f457df1de0af80533b5132b958d90c1b
jenil-ux <savaliyajenil501@gmail.com>
2026-06-18 11:27:52 +0530

    Add scroll animation using IntersectionObserver

---
+++ scrollanimation.js
@@ -0,0 +1,24 @@
+const observer=
+new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+});
+
+document
+.querySelectorAll(".card")
+.forEach(el=>{
+
+el.classList.add("hidden");
+
+observer.observe(el);
+
+});
